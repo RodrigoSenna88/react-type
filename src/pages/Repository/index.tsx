01a4780396a352d2c7a76cf9f58f18c677cb54bf
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-11-27 19:48:40 -0300

    aula - estilizando detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,66 @@
 import React from 'react';
-import { useRouteMatch }from 'react-router-dom'; 
+import { useRouteMatch, Link }from 'react-router-dom'; 
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
 
 interface RepositoryParams {
   repository: string;
 }
 
+
 const Repository: React.FC= () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+  <>
+    <Header>
+      <img src={logoImg} alt="Github Explorer" />
+      <Link to="/">
+        <FiChevronLeft size={16}/>
+        Voltar
+      </Link>
+    </Header>
+
+    <RepositoryInfo>
+      <header>
+        <img src="https://avatars2.githubusercontent.com/u/56227526?s=460&u=2074ddcc5a2198c3778cdc643518afcd5ee67c7e&v=4" alt="RodrigoSenna"/>
+        <div>
+          <strong>rodrigosenna88/GBTYPE</strong>
+          <p>descrição do repositório</p>
+        </div>
+      </header>
+      <ul>
+        <li>
+          <strong>1808</strong>
+          <span>Stars</span>
+        </li>
+        <li>
+          <strong>48</strong>
+          <span>Forks</span>
+        </li>
+        <li>
+          <strong>67</strong>
+          <span>Issues abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
+
+    <Issues>
+    <Link to={"asd"}>
+       
+        <div>
+        <strong>asdf</strong>
+        <p>asdf</p>
+        </div>
+        
+        <FiChevronRight size={20}/>
+        </Link>
+    </Issues>
+  </>
+  );
 }
 
 export default Repository;
